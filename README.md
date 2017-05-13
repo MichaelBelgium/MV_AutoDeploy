cbe7d042ed21f83e7becc90e06405effdf3614ee
MichaelBelgium <michaelvrld@gmail.com>
2017-05-13 15:30:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SAMP-Auto-Update-Git
---
+++ deploy.php
@@ -0,0 +1,14 @@
+<?php
+    class Config
+    {
+        public static $MYSQL_HOST = "127.0.0.1";
+        public static $MYSQL_USER = "";
+        public static $MYSQL_PASS = "";
+        public static $MYSQL_DB = "";
+
+        public static $SSH_HOST = "127.0.0.1";
+        public static $SSH_USER = "";
+        public static $SSH_PASS = "";
+        public static $SSH_GIT_DIR = "";
+    }
+?>
