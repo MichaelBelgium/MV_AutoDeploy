644d25f7e1736bb200d45ac2ddb4dd4e25712db9
MichaelBelgium <michaelvrld@gmail.com>
2017-05-13 19:42:40 +0200

    Add table.sql

---
+++ table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `Update_Data` (
+  `uID` int(11) NOT NULL AUTO_INCREMENT,
+  `Hash` varchar(256) NOT NULL,
+  `Message` varchar(64) NOT NULL,
+  `Branch` varchar(32) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`uID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
