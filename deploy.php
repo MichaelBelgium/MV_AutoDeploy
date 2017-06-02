3ce978b57d5e620d217ef4e1c0573c1e493fc956
MichaelBelgium <michaelvrld@gmail.com>
2017-06-02 22:05:22 +0200

    Eliminate repetitive code

--- deploy.php
+++ deploy.php
@@ -43,38 +43,27 @@
     $message = $con->real_escape_string($message);
     $con->query("INSERT INTO Update_Data (Hash, Message, Branch, Date) VALUES ('$hash', '$message', '$branch', '$date')");
 
+    $ssh = ssh2_connect(Config::$SSH_HOST);
+    ssh2_auth_password($ssh, Config::$SSH_USER, Config::$SSH_PASS);
+
     if($branch === "master")
     {
         echo "Deploying to vps (live server) ...";
 
-        $ssh = ssh2_connect(Config::$SSH_HOST);
-        ssh2_auth_password($ssh, Config::$SSH_USER, Config::$SSH_PASS);
-
         $str = ssh2_exec($ssh,"cd ".Config::$SSH_GIT_DIR. " && git pull origin master");
-
-        // $errstr = ssh2_fetch_stream($str, SSH2_STREAM_STDERR);
-        // stream_set_blocking($str, true);
-        // stream_set_blocking($errstr, true);
-        // echo "| Output: " . stream_get_contents($str);
-        // echo "| Error: " . stream_get_contents($errstr);
-
-        echo "Done!";
     }
     else if($branch === Config::$DEV_BRANCH)
     {
         echo "Deploying to vps (test server) ...";
 
-        $ssh = ssh2_connect(Config::$SSH_HOST);
-        ssh2_auth_password($ssh, Config::$SSH_USER, Config::$SSH_PASS);
-
         $str = ssh2_exec($ssh,"cd ".Config::$SSH_TEST_GIT_DIR. " && git pull origin ". Config::$DEV_BRANCH);
-
-        // $errstr = ssh2_fetch_stream($str, SSH2_STREAM_STDERR);
-        // stream_set_blocking($str, true);
-        // stream_set_blocking($errstr, true);
-        // echo "| Output: " . stream_get_contents($str);
-        // echo "| Error: " . stream_get_contents($errstr);
-
-        echo "Done!";
     }
-?>
+
+	// $errstr = ssh2_fetch_stream($str, SSH2_STREAM_STDERR);
+	// stream_set_blocking($str, true);
+	// stream_set_blocking($errstr, true);
+	// echo "| Output: " . stream_get_contents($str);
+	// echo "| Error: " . stream_get_contents($errstr);
+
+    echo "Done!";
+ ?>
