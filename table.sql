1671e6389a22a181e577f13be0f6236141964f3c
MichaelBelgium <michaelvrld@gmail.com>
2017-06-17 19:34:51 +0200

    No need to use a file. (part 2)

--- table.sql
+++ table.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS `Update_Data` (
   `Message` varchar(64) NOT NULL,
   `Branch` varchar(32) NOT NULL,
   `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Handled` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`uID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
